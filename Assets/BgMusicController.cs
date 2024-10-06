2a8522ee8173a5b72843008fe68452465b32fa36
jessica-px <jessypeck@gmail.com>
2024-10-06 13:05:30 -0700

    Add bg music and fade controller

---
+++ Assets/BgMusicController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgMusicController : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        StartCoroutine(StartFade(0, 1, 1));
+    }
+
+    public IEnumerator StartFade(float startVolume, float targetVolume, float duration)
+    {
+        float currentTime = 0;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
+    }
+}
